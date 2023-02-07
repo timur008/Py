5fa7ff7c87c96540f0498cc22e95aa903b9cd81c
Пользователь <testlemanul229@gmail.com>
2023-02-07 18:03:44 +0200

    less5

---
+++ less5.py
@@ -0,0 +1,18 @@
+# import requests
+# import inspect
+#
+# intro_list = []
+# for method in dir(intro_list):
+#     print(method)
+#
+# intro_list.count()
+#
+# data = "string"
+#
+# print(hasattr(data, "reverse"))
+# print(hasattr(data, "index"))
+# data = "string"
+#
+#
+# print(getattr(data, "startswith", None))
+# print(getattr(data, "reverse", None))
