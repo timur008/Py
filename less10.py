9130f799acfe21130f8225e0f87229ef9f21af06
Пользователь <testlemanul229@gmail.com>
2023-02-23 17:40:50 +0200

    ok

--- less10.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("E:/itstep_DB.sl3", 5)
-cur = connection.cursor()
-cur.execute("CREATE TABLE first_table (name TEXT);")
-connection.commit()
-connection.close()
