0ce46ed8e0821bfefe433cac0b688cb163e5e950
Пользователь <testlemanul229@gmail.com>
2023-03-01 22:32:33 +0200

    ok

--- hw6.py
+++ hw6.py
@@ -1,13 +1,18 @@
 result = []
+
 def divider(a, b):
-    if a < b:
-        raise ValueError
-    if b > 100:
-        raise IndexError
+    # if b == 0:
+    #     raise ZeroDivisionError
+    # if a != type(int):
+    #     raise TypeError("kek")
+    # else:
     return a/b
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+
+data = {10: 2, 2: 5, 18: 0, 8: 4}
 for key in data:
     res = divider(key, data[key])
     result.append(res)
+
+
 
 print(result)
