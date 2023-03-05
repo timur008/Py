a8f3fc7ab21a00b566f27fe29ad7734dddfbf909
Пользователь <testlemanul229@gmail.com>
2023-03-05 16:41:59 +0200

    ok

--- hw7.py
+++ hw7.py
@@ -1,2 +1,20 @@
+def checker(*exc_types):
+    def checker(function):
+        def checker(*args, **kwargs):
+            try:
+                result = function(*args, **kwargs)
+            except (exc_types) as exc:
+                print(f"We have problems {exc}")
+            else:
+                print(f"No problems. Result - {result}")
+        return checker
+    return checker
+
+@checker(NameError, TypeError, SyntaxError)
 def calculate(expression):
     return eval(expression)
+
+a = int(input("Enter first number - "))
+b = int(input("Enter second number - "))
+c = int(input("Enter operation - "))
+calculate(a, c, b)
